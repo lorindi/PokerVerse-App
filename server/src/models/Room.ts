b2eb7518fe599d77d5bedc43c761d84e50cf8552
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-02-27 17:57:13 +0200

    Create Room.ts

---
+++ server/src/models/Room.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, mongo, Schema } from "mongoose";
+
+export interface IRoom extends Document {
+  name: string;
+  maxPlayers: number;
+  createdAt: Date;
+  players: mongoose.Types.ObjectId[];
+}
+
+const RoomSchema = new Schema<IRoom>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  maxPlayers: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  players: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Player",
+    },
+  ],
+});
+RoomSchema.index({ name: 1 });
+
+export default mongoose.model<IRoom>("Room", RoomSchema);
